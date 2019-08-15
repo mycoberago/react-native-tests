691f9b212f93224ec1210eadb331e7ceb63ed815
Mike Berago <mycoberago@gmail.com>
2019-08-14 22:16:42 -0500

    market_item_details renders the title, price, details and a button to message user

--- app/components/market_item_details.component.js
+++ app/components/market_item_details.component.js
@@ -10,6 +10,7 @@ import {
   Dimensions,
   Image
 } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
@@ -23,6 +24,7 @@ const MarketItemDetailsComponent = (props) => {
   return(
     <View style={styles.container}>
       <SafeAreaView>
+
         <FlatList
           data={item.images}
           renderItem={(image) =>
@@ -36,7 +38,15 @@ const MarketItemDetailsComponent = (props) => {
           horizontal={true}
         />
 
-        <Text>{item.title}</Text>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.price}><Text style={styles.price_label}>Price:</Text> {"$" + (item.price /100).toFixed(2)}</Text>
+        <Text style={styles.desc}>{item.desc}</Text>
+        <Button
+          raised
+          title="Message User"
+          containerStyle={styles.buttonContainer}
+        />
+
       </SafeAreaView>
     </View>
   )
@@ -51,6 +61,23 @@ const styles = StyleSheet.create({
     height: deviceHeight * .25,
     width: deviceWidth * .80,
     margin: deviceWidth * .025
+  },
+  title: {
+    fontSize: deviceWidth * .05,
+    fontWeight: 'bold',
+    padding: deviceWidth * .025
+  },
+  desc: {
+    padding: deviceWidth * .025
+  },
+  price: {
+    padding: deviceWidth * .025
+  },
+  price_label: {
+    fontWeight: 'bold'
+  },
+  buttonContainer: {
+    marginHorizontal: deviceWidth * .05
   }
 });
 
